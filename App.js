9747f4135535ea1dd80c5b2abd0153053068b1f1
7hourspg <rajiv840704@gmail.com.com>
2022-10-22 16:20:28 +0530

    Native-Route

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StatusBar } from "react-native";
+import * as React from "react";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          
+          }}
+        >
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              title: "Login",
+              headerTitleAlign: "center",
+              headerStyle: {
+                backgroundColor: "#f4511e",
+              },
+            }}
+          />
+          <Stack.Screen name="signup" component={Signup}
+           />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ pages/Login.js
@@ -0,0 +1,78 @@
+import { View, Text, Image, StyleSheet, TextInput } from "react-native";
+import React, { useState } from "react";
+import logo from "../assets/whale-7446905_960_720.jpg";
+const Login = ({navigation}) => {
+  const [first, setfirst] = useState("");
+  console.log(first);
+  return (
+    <>
+      <View>
+        <View>
+          <Image source={logo} style={styles.Image} />
+        </View>
+        <View style={styles.text_container}>
+          <Text style={styles.text}>Login</Text>
+        </View>
+        <View>
+          <TextInput
+            placeholder="Email"
+            style={styles.input}
+            onChangeText={setfirst}
+          />
+          <TextInput placeholder="Password" style={styles.input} />
+          <Text onPress={() => console.log("Hello")} style={styles.btn}>
+           Submit
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.not_account}>
+             Don't have account ? <Text style={styles.span} onPress={() => navigation.navigate('signup',{id:1})}>Sign Up</Text>
+          </Text>
+        </View>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  Image: {
+    width: 250,
+    height: 250,
+    borderRadius: 1000,
+    marginTop: 20,
+   alignSelf:'center'
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 20,
+  },
+  text_container: {
+    alignSelf: "center",
+    justifyContent: "space-around",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  btn: {
+    backgroundColor: "black",
+    color: "white",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    alignSelf: "center",
+    marginTop: 10,
+    borderRadius: 5,
+  },
+  not_account: {
+    alignSelf: "center",
+    marginTop: 20,
+  },
+  span: {
+    color: "red",
+  },
+});
+
+export default Login;
