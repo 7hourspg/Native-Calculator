ac522afe32258fefa71f76e81129114195eb7a74
7hourspg <rajiv840704@gmail.com.com>
2022-11-03 13:02:04 +0530

    Calculator

--- App.js
+++ App.js
@@ -1,107 +1,439 @@
 import {
   View,
   Text,
+  StatusBar,
+  Button,
   Dimensions,
-  Image,
-  StyleSheet,
-  FlatList,
-  Animated,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
 } from "react-native";
-import React from "react";
-import react from "react";
-
+import React, { useState, useEffect } from "react";
+const { width, height } = Dimensions.get("screen");
+const color = "white";
 const App = () => {
-  const { width, height } = Dimensions.get("screen");
-  const Item_Width = width * 0.76;
-  const Item_Height = width * 1.47;
-  const Images = [
-    "https://images.unsplash.com/photo-1605092676920-8ac5ae40c7c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80",
-    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-    "https://images.unsplash.com/photo-1503656142023-618e7d1f435a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=467&q=80",
-  ];
-  const data = Images.map((image, index) => ({
-    key: String(index),
-    photo: image,
-  }));
-  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const [arr, setArr] = useState([]);
+  const [arr2, setArr2] = useState([]);
+  const [value1, setValue1] = useState();
+  const [value2, setValue2] = useState();
+  const [operator, setOperator] = useState();
+  const [isTrue, setIsTrue] = useState(false);
+  const [data, setData] = useState(null);
+
+  // Handle String
+
+  const handleData = (num) => {
+    return isTrue ? setArr2([...arr2, num]) : setArr([...arr, num]);
+  };
+
+  useEffect(() => {
+    setValue1(
+      Math.floor(arr.reduce((partialSum, a) => partialSum + a, arr ? "" : "0"))
+    );
+    setValue2(
+      Math.floor(
+        arr2.reduce((partialSum, a) => partialSum + a, arr2 ? "" : "0")
+      )
+    );
+  }, [arr2, data, arr]);
+
+  //Handle operator
+
+  useEffect(() => {
+    if (operator === 1 && 2 && 3) {
+      setIsTrue(true);
+    }
+  }, [operator]);
+  // Handle Enter
+  const handleEnter = () => {
+    if (operator === 1) {
+      function add() {
+        if (data === null) {
+          return setData(value1 + value2), setArr([]), setArr2([]);
+        } else {
+          return setData(data + value2), setArr2([]);
+        }
+      }
+      add();
+    }
+    if (operator === 2) {
+      function Minus() {
+        if (data === null) {
+          return setData(value1 - value2), setArr([]), setArr2([]);
+        } else {
+          return setData(data - value2), setArr2([]);
+        }
+      }
+      Minus();
+    }
+
+    if (operator === 3) {
+      function Multiply() {
+        if (data === null) {
+          return setData(value1 * value2), setArr([]), setArr2([]);
+        } else {
+          return setData(data * value2), setArr2([]);
+        }
+      }
+      Multiply();
+    }
+  };
+
+  console.log(value1, "value1");
+  console.log(value2, "value2");
+  console.log(data, "data");
   return (
-    <View style={styles.container}>
-      <Animated.FlatList
-        data={data}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        pagingEnabled
-        onScroll={Animated.event(
-          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-          { useNativeDriver: true }
-        )}
-        keyExtractor={(item) => item.key}
-        renderItem={({ item, index }) => {
-          const inputRange = [
-            (index - 1) * width,
-            index * width,
-            (index + 1) * width,
-          ];
-          const translateX = scrollX.interpolate({
-            inputRange,
-            outputRange: [-width * 0.7, 0, width * 0.7],
-          });
-          return (
+    <>
+      <StatusBar />
+      <TouchableWithoutFeedback
+        onPress={() => Keyboard.dismiss()}
+        accessible={false}
+      >
+        <View style={{ flex: 1, backgroundColor: "black", width, height }}>
+          <View
+            style={{
+              height: height / 3,
+              alignItems: "flex-end",
+              flexDirection: "row",
+              justifyContent: "flex-end",
+              width: width - 20,
+            }}
+          >
             <View
-              style={{ width, justifyContent: "center", alignItems: "center" }}
+              style={{
+                justifyContent: "space-evenly",
+                alignItems: "center",
+                width: width - 40,
+                height: height / 3,
+              }}
             >
-              <View
+              <Text
                 style={{
-                  borderRadius: 18,
-                  shadowColor: "red",
-                  shadowRadius: 30,
-                  shadowOpacity: 1,
-                  shadowOffset: {
-                    width: 0,
-                    height: 0,
-                  },
-                  padding: 12,
-                  backgroundColor: "white",
-                  elevation: 20,
+                  color,
+                  fontSize: 30,
+                  marginRight: 10,
+                  marginBottom: 50,
+                  textAlign: "center",
                 }}
               >
-                <View
+                TOTAL : {data}
+              </Text>
+              <Text
+                style={{
+                  color,
+                  fontSize: 60,
+                  marginRight: 10,
+                  marginBottom: 50,
+                  textAlign: "left",
+                }}
+              >
+                {value1}
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              width: width - 20,
+              alignSelf: "center",
+              flexDirection: "row",
+              justifyContent: "space-around",
+              alignItems: "stretch",
+              flexWrap: "wrap",
+            }}
+          >
+            <TouchableOpacity onPress={() => handleData(1)}>
+              <View>
+                <Text
                   style={{
-                    width: Item_Width,
-                    height: Item_Height - 50,
-                    overflow: "hidden",
-                    alignItems: "center",
-                    borderRadius: 14,
+                    color,
+                    backgroundColor: "#474E68",
+                    borderRadius: 100,
+                    fontSize: 35,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    paddingHorizontal: 28,
+                    paddingVertical: 15,
+                    opacity: 0.8,
                   }}
                 >
-                  <Animated.Image
-                    source={{ uri: item.photo }}
-                    style={{
-                      width: Item_Width * 1.4,
-                      height: Item_Height,
-                      resizeMode: "cover",
-                      transform: [
-                        {
-                          translateX,
-                        },
-                      ],
-                    }}
-                  />
-                </View>
+                  1
+                </Text>
               </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleData(2)}>
+              <View>
+                <Text
+                  style={{
+                    color,
+                    backgroundColor: "#474E68",
+                    borderRadius: 100,
+                    fontSize: 35,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    paddingHorizontal: 28,
+                    paddingVertical: 15,
+                    opacity: 0.8,
+                  }}
+                >
+                  2
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleData(3)}>
+              <View>
+                <Text
+                  style={{
+                    color,
+                    backgroundColor: "#474E68",
+                    borderRadius: 100,
+                    fontSize: 35,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    paddingHorizontal: 28,
+                    paddingVertical: 15,
+                    opacity: 0.8,
+                  }}
+                >
+                  3
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setOperator(1)}>
+              <View>
+                <Text
+                  style={{
+                    color,
+                    backgroundColor: "#FF7F3F",
+                    borderRadius: 100,
+                    fontSize: 40,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    paddingHorizontal: 28,
+                    paddingVertical: 12,
+                    opacity: 0.8,
+                  }}
+                >
+                  +
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View>
+                <Text
+                  style={{
+                    color,
+                    backgroundColor: "#474E68",
+                    borderRadius: 100,
+                    fontSize: 35,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    paddingHorizontal: 28,
+                    paddingVertical: 15,
+                    marginVertical: 20,
+                    opacity: 0.8,
+                  }}
+                >
+                  4
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View>
+                <Text
+                  style={{
+                    color,
+                    backgroundColor: "#474E68",
+                    borderRadius: 100,
+                    fontSize: 35,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    paddingHorizontal: 28,
+                    paddingVertical: 15,
+                    marginVertical: 20,
+                    opacity: 0.8,
+                  }}
+                >
+                  5
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View>
+                <Text
+                  style={{
+                    color,
+                    backgroundColor: "#474E68",
+                    borderRadius: 100,
+                    fontSize: 35,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    paddingHorizontal: 28,
+                    paddingVertical: 15,
+                    marginVertical: 20,
+                    opacity: 0.8,
+                  }}
+                >
+                  6
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setOperator(2)}>
+              <View>
+                <Text
+                  style={{
+                    color,
+                    backgroundColor: "#FF7F3F",
+                    borderRadius: 100,
+                    fontSize: 40,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    paddingHorizontal: 30,
+                    paddingVertical: 12,
+                    marginVertical: 20,
+                    opacity: 0.8,
+                  }}
+                >
+                  -
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <View>
+              <Text
+                style={{
+                  color,
+                  backgroundColor: "#474E68",
+                  borderRadius: 100,
+                  fontSize: 35,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  paddingHorizontal: 28,
+                  paddingVertical: 15,
+                  marginVertical: 20,
+                  opacity: 0.8,
+                }}
+              >
+                7
+              </Text>
             </View>
-          );
-        }}
-      />
-    </View>
+            <View>
+              <Text
+                style={{
+                  color,
+                  backgroundColor: "#474E68",
+                  borderRadius: 100,
+                  fontSize: 35,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  paddingHorizontal: 28,
+                  paddingVertical: 15,
+                  marginVertical: 20,
+                  opacity: 0.8,
+                }}
+              >
+                8
+              </Text>
+            </View>
+            <View>
+              <Text
+                style={{
+                  color,
+                  backgroundColor: "#474E68",
+                  borderRadius: 100,
+                  fontSize: 35,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  paddingHorizontal: 28,
+                  paddingVertical: 15,
+                  marginVertical: 20,
+                  opacity: 0.8,
+                }}
+              >
+                9
+              </Text>
+            </View>
+            <TouchableOpacity onPress={() => setOperator(3)}>
+              <View>
+                <Text
+                  style={{
+                    color,
+                    backgroundColor: "#FF7F3F",
+                    borderRadius: 100,
+                    fontSize: 40,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    paddingHorizontal: 28,
+                    paddingVertical: 12,
+                    marginVertical: 20,
+                    opacity: 0.8,
+                  }}
+                >
+                  *
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <View>
+              <Text
+                style={{
+                  color,
+                  backgroundColor: "#474E68",
+                  borderRadius: 100,
+                  fontSize: 35,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  paddingHorizontal: 75,
+                  paddingVertical: 15,
+                  marginVertical: 20,
+                  opacity: 0.8,
+                }}
+              >
+                0
+              </Text>
+            </View>
+            <View>
+              <Text
+                style={{
+                  color,
+                  backgroundColor: "#474E68",
+                  borderRadius: 100,
+                  fontSize: 35,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  paddingHorizontal: 35,
+                  paddingVertical: 15,
+                  marginVertical: 20,
+                  opacity: 0.8,
+                }}
+              >
+                .
+              </Text>
+            </View>
+            <TouchableOpacity onPress={() => handleEnter()}>
+              <View>
+                <Text
+                  style={{
+                    color,
+                    backgroundColor: "#FF7F3F",
+                    borderRadius: 100,
+                    fontSize: 40,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    paddingHorizontal: 28,
+                    paddingVertical: 12,
+                    marginVertical: 20,
+                    opacity: 0.8,
+                  }}
+                >
+                  =
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    justifyContent: "center",
-  },
-});
 export default App;
